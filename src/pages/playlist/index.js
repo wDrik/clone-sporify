7568b4b4d9df19f9e63ed686e2fc69c911dc9a35
Iorgen <w.drik@outlook.com>
2018-12-25 18:23:22 -0200

    Dinamic playslist page!

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -1,81 +1,120 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as PlaylistDetailsActions } from '../../store/ducks/playlistDetails';
 
 import { Container, Header, SongList } from './styles';
+
+import Loading from '../../components/Loading';
 
 import ClockIcon from '../../assets/images/clock.svg';
 import PlusIcon from '../../assets/images/plus.svg';
 
-const Playlist = () => (
-  <Container>
-    <Header>
-      <img
-        src="https://f4.bcbits.com/img/a2211851590_10.jpg"
-        alt="Playlist"
-      />
+class Playlist extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        id: PropTypes.string,
+      }),
+    }).isRequired,
+    getPlaylistDetailsRequest: PropTypes.func.isRequired,
+    playlistDetails: PropTypes.shape({
+      data: PropTypes.shape({
+        id: PropTypes.number,
+        title: PropTypes.string,
+        thumbnail: PropTypes.string,
+        description: PropTypes.string,
+        songs: PropTypes.arrayOf(PropTypes.shape({
+          id: PropTypes.number,
+          title: PropTypes.string,
+          author: PropTypes.string,
+          album: PropTypes.string,
+        }))
+      }),
+      loading: PropTypes.bool,
+    }).isRequired,
+  }
 
-      <div>
-        <span>Playlist</span>
+  componentDidMount() {
+    this.loadPlaylistDetails();
+  }
 
-        <h1>Rock 4ever</h1>
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.id !== this.props.match.params.id)
+      this.loadPlaylistDetails();
+  }
 
-        <p>12 músicas</p>
+  loadPlaylistDetails = () => {
+    const { id } = this.props.match.params;
 
-        <button>Play</button>
-      </div>
-    </Header>
+    this.props.getPlaylistDetailsRequest(id);
+  }
 
-    <SongList cellPadding={0} cellSpacing={0}>
-      <thead>
-        <th />
-        <th>Título</th>
-        <th>Artista</th>
-        <th>Álbum</th>
-        <th><img src={ClockIcon} alt="Duração" /></th>
-      </thead>
+  renderDetails = () => {
+    const playlist = this.props.playlistDetails.data;
 
-      <tbody>
-        <tr>
-          <td><img src={PlusIcon} alt="Adicionar" /></td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
+    return (
+      <Container>
+        <Header>
+          <img
+            src={playlist.thumbnail}
+            alt={playlist.title}
+          />
 
-        <tr>
-          <td><img src={PlusIcon} alt="Adicionar" /></td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
+          <div>
+            <span>Playlist</span>
+            <h1>{playlist.title}</h1>
+            { !!playlist.songs && <p>{playlist.songs.length} {playlist.songs.length === 1 ? 'música' : 'músicas'}</p> }
+            <button>Play</button>
+          </div>
+        </Header>
 
-        <tr>
-          <td><img src={PlusIcon} alt="Adicionar" /></td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
+        <SongList cellPadding={0} cellSpacing={0}>
+          <thead>
+            <tr>
+              <th />
+              <th>Título</th>
+              <th>Artista</th>
+              <th>Álbum</th>
+              <th><img src={ClockIcon} alt="Duração" /></th>
+            </tr>
+          </thead>
 
-        <tr>
-          <td><img src={PlusIcon} alt="Adicionar" /></td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
+          <tbody>
+            {!playlist.songs ? (
+              <tr>
+                <td colSpan={5}>Nenhuma música cadastrada</td>
+              </tr>
+            ) : (
+              playlist.songs.map(song => (
+                <tr key={song.id}>
+                  <td><img src={PlusIcon} alt="Adicionar" /></td>
+                  <td>{song.title}</td>
+                  <td>{song.author}</td>
+                  <td>{song.album}</td>
+                  <td>3:26</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </SongList>
+      </Container>
+    );
+  }
 
-        <tr>
-          <td><img src={PlusIcon} alt="Adicionar" /></td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
-      </tbody>
-    </SongList>
-  </Container>
-);
+  render() {
+    return this.props.playlistDetails.loading ?
+      <Container loading><Loading /></Container> :
+      this.renderDetails();
+  }
+}
 
-export default Playlist;
+const mapStateToProps = state => ({
+  playlistDetails: state.playlistDetails,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(PlaylistDetailsActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Playlist);
--- src/store/ducks/playlistDetails.js
+++ src/store/ducks/playlistDetails.js
@@ -1,14 +1,14 @@
 export const Types = {
-  GET_REQUEST: 'playlists/GET_REQUEST',
-  GET_SUCCESS: 'playlists/GET_SUCCESS',
+  GET_REQUEST: 'playlistDetails/GET_REQUEST',
+  GET_SUCCESS: 'playlistDetails/GET_SUCCESS',
 }
 
 const INITIAL_STATE = {
-  data: [],
+  data: {},
   loading: false,
 }
 
-export default function playlists(state = INITIAL_STATE, action) {
+export default function playlistDetails(state = INITIAL_STATE, action) {
   switch (action.type) {
     case Types.GET_REQUEST:
       return { ...state, loading: true }
@@ -20,9 +20,12 @@ export default function playlists(state = INITIAL_STATE, action) {
 }
 
 export const Creators = {
-  getPlaylistsRequest: () => ({ type: Types.GET_REQUEST }),
+  getPlaylistDetailsRequest: id => ({
+    type: Types.GET_REQUEST,
+    payload: { id },
+  }),
 
-  getPlaylistsSuccess: data => ({
+  getPlaylistDetailsSuccess: data => ({
     type: Types.GET_SUCCESS,
     payload: { data }
   }),
